db67135b63e3a91a5e866b222088f970561bbf04
Saabat <ashiq.eee.aiub@gmail.com>
2022-12-11 16:50:22 +0600

    price converted to BDT

--- mobile_1.py
+++ mobile_1.py
@@ -17,7 +17,10 @@ i = 0
 while i < len(mobile_data['data']):
     name = mobile_data.get('data')[i].get('name')
     price = mobile_data.get('data')[i].get('price')
+    price_split = price.split()
+    price_float = float(price_split[0])
+    exchange_rate = mobile_data.get('exchnage_rate')
     made = mobile_data.get('data')[i].get('made')
-    sentence = f"{name} is made in {made}. The price is {price} which is equal to "
+    sentence = f"{name} is made in {made}. The price is {price} which is equal to {exchange_rate * price_float} BDT "
     i = i + 1
     print(sentence)
