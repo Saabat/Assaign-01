e72babc75a520d16acd39e9a33ca8e3cbe4b11de
Saabat <ashiq.eee.aiub@gmail.com>
2022-12-11 11:48:05 +0600

    commit

---
+++ mobile_1.py
@@ -0,0 +1,23 @@
+mobile_data = {
+    'status': True,
+    'data': [
+        {'name': 'Xiaomi Note 5', 'price': '300 USD', 'made': 'China'},
+        {'name': 'Samsung Note 6', 'price': '200 USD', 'made': 'USA'},
+        {'name': 'Iphone 5', 'price': '180.5 USD', 'made': 'Japan'},
+        {'name': 'Pixel 5', 'price': '89.60 USD', 'made': 'Rusia'},
+        {'name': 'Techno 5', 'price': '110 USD', 'made': 'Uk'},
+        {'name': 'Huawei 5', 'price': '350 USD', 'made': 'Malaysia'},
+    ],
+    'exchnage_rate': 103.25
+}
+
+#  Your Code Starts from here
+
+i = 0
+while i < len(mobile_data['data']):
+    name = mobile_data.get('data')[i].get('name')
+    price = mobile_data.get('data')[i].get('price')
+    made = mobile_data.get('data')[i].get('made')
+    sentence = f"{name} is made in {made}. The price is {price} which is equal to "
+    i = i + 1
+    print(sentence)
